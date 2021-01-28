3641b92610128a4b5d3515f1c55faa7774828a61
Youngchan-K <kyc9618@gmail.com>
2021-01-28 16:21:40 +0900

    블루투스 연결

--- BluetoothConnection.cpp
+++ BluetoothConnection.cpp
@@ -1,9 +1,18 @@
+// g++ -o Bluetooth BluetoothConnection.cpp -lbluetooth -> ./Bluetooth
+// 리눅스에서 컴파일하고 실행하면, Segmentation Fault가 발생할 수 있음
+/*
+sudo nano /etc/systemd/system/dbus-org.bluez.service
+ExecStart=/usr/lib/bluetooth/bluetoothd 뒤에 '(앞에 1칸 띄어쓰기)--compat 추가'
+
+sudo nano ~/.bashrc 입력 후, 파일 끝에 sudo chmod 777 /var/run/sdp 추가
+*/
+
 #include "BluetoothConnection.h"
 
 bdaddr_t bdaddr_any = {0, 0, 0, 0, 0, 0};
 bdaddr_t bdaddr_local = {0, 0, 0, 0xff, 0xff, 0xff};
 
-// 블루투스 COM(직렬) 포트 사용할 수 있도록 UUID 설정
+// 블루투스 포트 사용할 수 있도록 UUID 설정
 sdp_session_t* register_service(uint8_t rfcomm_channel)
 {
     uint32_t service_uuid_int[] = {0x01110000, 0x00100000, 0x80000080, 0xFB349B5F};
@@ -26,36 +35,36 @@ sdp_session_t* register_service(uint8_t rfcomm_channel)
     sdp_record_t record = {0};
     sdp_session_t *session = 0;
 
-    // set the general service ID
+    // General service ID
     sdp_uuid128_create(&service_uuid, &service_uuid_int);
     sdp_set_service_id(&record, service_uuid);
 
-    char str[256] = "";
-    sdp_uuid2strn(&service_uuid, str, 256);
-    printf("Registering UUID %s\n", str);
+    char str_uuid[256] = "";
+    sdp_uuid2strn(&service_uuid, str_uuid, 256);
+    printf("Registering UUID %s\n", str_uuid);
 
-    // set the service class
+    // Service class
     sdp_uuid16_create(&service_class_uuid, SERIAL_PORT_SVCLASS_ID);
-    service_class_list = sdp_list_append(0, &service_class_uuid);
+    service_class_list = sdp_list_append(0, &service_class_uuid); 
     sdp_set_service_classes(&record, service_class_list);
 
-    // set the Bluetooth profile information
+    // Bluetooth profile information
     sdp_uuid16_create(&profile.uuid, SERIAL_PORT_PROFILE_ID);
     profile.version = 0x0100;
     profile_list = sdp_list_append(0, &profile);
     sdp_set_profile_descs(&record, profile_list);
 
-    // make the service record publicly browsable
+    // Make the service record publicly browsable
     sdp_uuid16_create(&root_uuid, PUBLIC_BROWSE_GROUP);
     root_list = sdp_list_append(0, &root_uuid);
-    sdp_set_browse_groups( &record, root_list );
+    sdp_set_browse_groups(&record, root_list);
 
-    // set l2cap information
+    // L2CAP information
     sdp_uuid16_create(&l2cap_uuid, L2CAP_UUID);
-    l2cap_list = sdp_list_append( 0, &l2cap_uuid );
-    proto_list = sdp_list_append( 0, l2cap_list );
+    l2cap_list = sdp_list_append(0, &l2cap_uuid);
+    proto_list = sdp_list_append(0, l2cap_list);
 
-    // register the RFCOMM channel for RFCOMM sockets
+    // Register the RFCOMM channel for RFCOMM sockets
     sdp_uuid16_create(&rfcomm_uuid, RFCOMM_UUID);
     channel = sdp_data_alloc(SDP_UINT8, &rfcomm_channel);
     rfcomm_list = sdp_list_append(0, &rfcomm_uuid);
@@ -65,14 +74,16 @@ sdp_session_t* register_service(uint8_t rfcomm_channel)
     access_proto_list = sdp_list_append(0, proto_list);
     sdp_set_access_protos(&record, access_proto_list);
 
-    // set the service name, provider(company), and description
+    // Service name, provider(company), and description
     sdp_set_info_attr(&record, name, company, description);
 
-    // connect to the local SDP server, register the service record,
+    // Connect to the local SDP server
     session = sdp_connect(&bdaddr_any, &bdaddr_local, SDP_RETRY_IF_BUSY);
+
+    // Register the service record
     sdp_record_register(session, &record, 0);
 
-    // cleanup
+    // Cleanup
     sdp_data_free(channel);
     sdp_list_free(l2cap_list, 0);
     sdp_list_free(rfcomm_list, 0);
@@ -102,7 +113,6 @@ int main(int argc, char *argv[])
     BTsocket = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
     // Local 설정
-    bdaddr_t tmp;
 
     Local.rc_family = AF_BLUETOOTH;
     Local.rc_bdaddr = bdaddr_any;
@@ -119,42 +129,49 @@ int main(int argc, char *argv[])
     }
 
     // Listen
-    if (listen(BTsocket, 5) < 0) // 연결 시도하는 클라이언트 최대 5개
+    if (listen(BTsocket, port) < 0) // 연결 시도하는 클라이언트 최대 5개
     {
         cout << "Listen Failed" << endl;
 
     }
     else
     {
-        cout << "Listen for connection" << endl;
+        cout << "Listen for connection..." << endl;
     }
 
-    while(1)
-    {
-        // Accept - 클라이언트 연결 요청 수락
-        client = accept(BTsocket, (struct sockaddr *)&Remote, &socket_len);
+    
+    // Accept - 클라이언트 연결 요청 수락
+    client = accept(BTsocket, (struct sockaddr *)&Remote, &socket_len);
 
-        if (client < 0)
-        {
-            cout << "Accept Failed" << endl;
-        }
-        else
-        {
-            cout << "Accept Success" << endl;
-        }
+    if (client < 0)
+    {
+        cout << "Accept Failed" << endl;
+    }
+    else
+    {
+        cout << "Accept Success" << endl;
+    }
             
-        // Read data - 클라이언트로부터 데이터 수신
+    // Read data - 클라이언트로부터 데이터 수신
+    do
+    {
+        memset(input, 0, sizeof(input));
         read_data = read(client, input, sizeof(input));
 
         if(read_data > 0) 
         {
             cout << "Received Data :" << input << endl;
+            
+            if(input == "F" || "B")
+            {
+
+            }
         }
         else
         {
             cout << "데이터 수신 실패" << endl;
         }
-    }
+    } while (read_data > 0); 
 
 
     // Close connection
