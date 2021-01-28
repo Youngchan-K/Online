e6a04d1e381748078f96baac922dc3320f338bae
Youngchan-K <kyc9618@gmail.com>
2021-01-28 10:58:46 +0900

    블루투스 연결

---
+++ BluetoothConnection.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// 리눅스 환경에서 sudo apt-get install libbluetooth-dev 설치 필요
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth.l2cap.h>
+#include <bluetooth/rfcomm.h>
+#include <bluetooth.h/sco.h>
+#include <bluetooth/sdp.h>
+#include <bluetooth/sdp_lib.h>
+
+
+
+using namespace std;
