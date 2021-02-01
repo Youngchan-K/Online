b9112788de9d1af271b5d3a62e8c62665ef77c7a
Youngchan-K <kyc9618@gmail.com>
2021-02-01 13:19:20 +0900

    블루투스 원격 제어

--- BluetoothControl.h
+++ BluetoothControl.h
@@ -1,12 +1,15 @@
+/******************************************************************************
+작성자 : chan
+목적 : 카트와 스마트폰을 Bluetooth로 연결해서 스마트폰 입력값에 따라 카트 이동
+사용방식 : Bluetooth를 사용할 수 있도록 UUID를 설정하여 카트와 연결
+중요사항 : Bluetooth를 이용하여 socket으로 통신하기 위해서 UUID 반드시 필요
+******************************************************************************/
+
 #ifndef BLUETOOTHCONTROL
 #define BLUETOOTHCONTROL
 
-#define PORT 5
-
+#include "../ATS_Com/ATSCom.h"
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 // 리눅스 환경에서 해당 라이브러리를 불러오지 못할 수도 있음
 // sudo apt-get install libbluetooth-dev 설치 필요
@@ -18,16 +21,21 @@
 #include <bluetooth/sdp.h>
 #include <bluetooth/sdp_lib.h>
 
-#include "../ATS_Com/ATSCom.h"
-
 using namespace std;
+
+#define PORT 1
 
 class BluetoothControl
 {
 private:
-    int turn_cart(double theta);
-    
+    ATSCom *com;
+
+    sdp_session_t* register_service(uint8_t rfcomm_channel);
     int go_straight(double distance);
-}
+    int turn_cart(double theta);
+
+public:
+    int run();
+};
 
 #endif
