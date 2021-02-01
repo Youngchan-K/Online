5523caebb3b628f9733ea78b5b17dbc3f1f90623
Youngchan-K <kyc9618@gmail.com>
2021-02-01 14:40:23 +0900

    블루투스 원격 제어

--- BluetoothControl.h
+++ BluetoothControl.h
@@ -1,9 +1,9 @@
-/******************************************************************************
+/********************************************************************************
 작성자 : chan
-목적 : 카트와 스마트폰을 Bluetooth로 연결해서 스마트폰 입력값에 따라 카트 이동
+목적 : 카트와 스마트폰을 Bluetooth로 연결하고, 스마트폰 입력값에 따라 카트 이동 및 회전
 사용방식 : Bluetooth를 사용할 수 있도록 UUID를 설정하여 카트와 연결
 중요사항 : Bluetooth를 이용하여 socket으로 통신하기 위해서 UUID 반드시 필요
-******************************************************************************/
+*********************************************************************************/
 
 #ifndef BLUETOOTHCONTROL
 #define BLUETOOTHCONTROL
@@ -15,7 +15,7 @@
 // sudo apt-get install libbluetooth-dev 설치 필요
 #include <sys/socket.h>
 #include <bluetooth/bluetooth.h>
-#include <bluetooth/l2cap.h>
+//
 #include <bluetooth/rfcomm.h>
 #include <bluetooth/sco.h>
 #include <bluetooth/sdp.h>
@@ -31,6 +31,7 @@ private:
     ATSCom *com;
 
     sdp_session_t* register_service(uint8_t rfcomm_channel);
+
     int go_straight(double distance);
     int turn_cart(double theta);
 
