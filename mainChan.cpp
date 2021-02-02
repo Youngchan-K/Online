b7ed70bd79b55c890c928dbe8710051701fb76d2
Youngchan-K <kyc9618@gmail.com>
2021-02-02 13:56:35 +0900

    실행 파일

--- mainChan.cpp
+++ mainChan.cpp
@@ -1,8 +1,15 @@
-#include "App/BluetoothControl.h"
+#include "BluetoothControl.h"
 
-// 컴파일&실행 : g++ -o Bluetooth mainChan.cpp BluetoothConnection.cpp -lbluetooth -> ./Bluetooth
-// Segmentation Fault가 발생할 수 있음
+// 컴파일&실행 : g++ -o Bluetooth mainChan.cpp ATSCom.cpp BluetoothConnection.cpp -lbluetooth -lpthread -> ./Bluetooth
+// Segmentation Fault가 발생할 수 있다.
+
 /*
+리눅스 환경에서 라이브러리를 불러오지 못할 수도 있음
+
+1. 블루투스 라이브러리 설치
+sudo apt-get install libbluetooth-dev
+
+2. SDP Server 활성화
 sudo nano /etc/systemd/system/dbus-org.bluez.service
 ExecStart=/usr/lib/bluetooth/bluetoothd 뒤에 '(앞에 1칸 띄어쓰기)--compat 추가'
 
