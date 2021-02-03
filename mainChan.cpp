55b76af807af94b2605c14427850aae778a8d063
Youngchan-K <kyc9618@gmail.com>
2021-02-03 13:00:33 +0900

    실행 파일

--- mainChan.cpp
+++ mainChan.cpp
@@ -1,32 +1,35 @@
+#include "ATSCom.h"
 #include "BluetoothControl.h"
 
 /*
-리눅스 환경에서 라이브러리를 불러오지 못할 수도 있음
-
 1. 블루투스 라이브러리 설치
-sudo apt-get install libbluetooth-dev
+- 리눅스 환경에서 라이브러리를 불러오지 못할 수도 있음
+- sudo apt-get install libbluetooth-dev
 
 2. SDP Server 활성화
-컴파일&실행 : g++ -o Bluetooth mainChan.cpp ATSCom.cpp BluetoothConnection.cpp -lbluetooth -lpthread -> ./Bluetooth
-Segmentation Fault가 발생할 수 있다.
+- 컴파일&실행 : g++ -o Bluetooth mainChan.cpp ATSCom.cpp BluetoothConnection.cpp -lbluetooth -lpthread -> ./Bluetooth
+- Segmentation Fault가 발생할 수 있다.
 
-sudo nano /etc/systemd/system/dbus-org.bluez.service
-ExecStart=/usr/lib/bluetooth/bluetoothd 뒤에 '(앞에 1칸 띄어쓰기)--compat 추가'
+- sudo nano /etc/systemd/system/dbus-org.bluez.service
+- ExecStart=/usr/lib/bluetooth/bluetoothd 뒤에 '(앞에 1칸 띄어쓰기)--compat 추가'
 
-sudo nano ~/.bashrc 입력 후, 파일 끝에 sudo chmod 777 /var/run/sdp 추가
+- sudo nano ~/.bashrc 입력 후, 파일 끝에 sudo chmod 777 /var/run/sdp 추가
 */
 
 int main()
 {
-    ATSCom atscom;
-    BluetoothControl BC;
+    ATSCom atscom("/dev/ttyUSB0", 115200, 0, 1);
+    BluetoothControl BC(&atscom);
     
+    uint8_t BTdata1 = 0;
+    uint8_t BTdata2 = 0;
 
     // 자동모드를 하기 위해 보드에 입력해주는 값
-    stateData1 = 5 | serialCom.ID.LEDState << 4; // or num | 0x60;
-    stateData2 = serialCom.ID.emergencyStop | serialCom.ID.callStaff << 1 | serialCom.ID.charingCableConnect << 2 | serialCom.ID.chareState << 3 | serialCom.ID.haptic << 4 | serialCom.ID.stationDocking << 6;
-	atscom.set_state(stateData1, stateData2);   
+    BTdata1 &= 0xF0; // or num | 0x60;
+    BTdata1 |= 6;
+	atscom.set_state(BTdata1, BTdata2);
 
+    // 블루투스 원격 제어 시작
     BC.run();
 
     return 0;
